72182e88e95c2537640fc0307f9aed118cc55bab
Yuusuf Jawad Olamide <yuusufjawadolamide67@gmail.com>
2026-02-10 13:50:45 +0100

    new commit

--- app/seller/page.tsx
+++ app/seller/page.tsx
@@ -25,14 +25,14 @@ const AddProduct = () => {
           <div className="flex flex-wrap items-center gap-3 mt-2">
 
             {[...Array(4)].map((_, index) => (
-              <label key={index} htmlFor={`image${index}`}>
+              <label key={index} htmlFor={`image₦{index}`}>
                 <input onChange={(e) => {
                   const updatedFiles = [...files];
                   if (e.target.files) {
                     updatedFiles[index] = e.target.files[0];
                     setFiles(updatedFiles);
                   }
-                }} type="file" id={`image${index}`} hidden />
+                }} type="file" id={`image₦{index}`} hidden />
                 <Image
                   key={index}
                   className="max-w-24 cursor-pointer"
